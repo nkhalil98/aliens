af8cf708e56291f8444ba16109c561f20b1f1da8
nkhalil98 <nabilamirkhalil@gmail.com>
2024-03-06 06:31:26 -0500

    add movement to aliens

--- src/aliens/settings.py
+++ src/aliens/settings.py
@@ -17,3 +17,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed = 2.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # 1 represents right; -1 represents left
